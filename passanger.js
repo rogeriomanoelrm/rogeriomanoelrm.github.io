9937ebc6a2d236a5bacf6a2d42c9ab52bd7e5759
unknown <rogerio>
2022-12-20 13:29:10 -0300

    update

--- passanger.js
+++ passanger.js
@@ -1,31 +1,20 @@
-
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+
 let count = 0
 
 function counter() {
-
     count += 1
     countEl.textContent = count
-
 }
 
-function save()   {
-
-    //let countStr = count + "-" caso eu não queira que volte do zero
-
-    let countStr = count + "-"
-
-    saveEl.innerText += countStr
+function save() {
+    let prevCount = saveEl.textContent ? parseInt(saveEl.textContent) : 0
+    saveEl.textContent = count + prevCount
 
     countEl.textContent = 0
-
     count = 0
-
-}
-
-
-    //console.log(count)
+} 
 
     
 
