02922929081be03314ac4e974e51fb8900f083aa
rogeriomanoelrm <77088268+rogeriomanoelrm@users.noreply.github.com>
2023-04-18 01:08:21 -0300

    Add files via upload

---
+++ passanger.js
@@ -0,0 +1,36 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let decrementEl = document.getElementById("decrement-el")
+
+
+let count = 0
+
+function counter() {
+    count += 1
+    countEl.textContent = count
+}
+
+function decrement() {
+    count--
+    countEl.textContent = count
+}
+
+
+
+
+function save() {
+    let prevCount = saveEl.textContent ? parseInt(saveEl.textContent) : 0
+    saveEl.textContent = count + prevCount
+
+    countEl.textContent = 0
+    count = 0
+
+
+} 
+
+
+
+
+
+
+    
